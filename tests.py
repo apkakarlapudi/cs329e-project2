3df8d1216b1239d642fb1bd5517f5cdcf6e1948c
juhipathak7 <juhipathak7@gmail.com>
2017-11-12 18:21:11 -0600

    Test

--- tests.py
+++ tests.py
@@ -1,20 +1,28 @@
 import unittest
 
+
 class TestTrivia(unittest.TestCase):
 
+	import main
+
     #Tests opening of file, correctly
-    def testCSVFormat(self):
-        pass
+	def testCSVFormat(self):
+		self.assertIsNot(myFile,null)
 
-    def testReadFile(self):
-    	pass
+#	def testReadFile(self):
+#		if data!= null:
+#			return True
 
-    def testScoreDisplay(self):
-    	pass
 
-    def testContinueGame(self):
-    	pass
+#	def testScoreDisplay(self):
+#		return self.score 
 
-    def testNotContinueGame(self):
-    	pass
-    
+  #  def testContinueGame(self):
+   # 	pass
+
+    #def testNotContinueGame(self):
+    #	pass
+
+
+if __name__ == '__main__':
+    unittest.main()
